e3444eacc09f9f396a7a33a663ed5948e3ead913
Daniel Fries <daniel.w.fries@gmail.com>
2019-02-14 10:54:34 -0500

    bills view, buttons work

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 const url = "http://localhost:3000/api/v1/representatives"
+const apiUrl = "https://api.propublica.org/congress/v1/116/house/bills/updated.json"
 
 document.addEventListener("DOMContentLoaded", (e)=>{
-  let centerContainer = document.querySelector("#center-feed")
-  let billButton = document.querySelector("#bill-btn")
-  let repButton = document.querySelector("#rep-btn")
+  const centerContainer = document.querySelector("#center-feed")
+  const billButton = document.querySelector("#bill-btn")
+  const repButton = document.querySelector("#rep-btn")
+  const billContainer = document.querySelector("#bill-container")
+  const repContainer = document.querySelector("#rep-container")
+  let fetchedReps = []
+  let fetchedBills = []
+
+
+  billButton.addEventListener("click", (e)=>{
+    console.log("bill button")
+    swapToBills()
+  })
+
+  repButton.addEventListener("click", (e)=>{
+    console.log("rep button")
+    swapToReps()
+  })
+
+
+  function fetchBills(){
+    fetch(apiUrl, {
+      method: "GET",
+      // mode: "no-cors",
+      headers: {
+        "X-API-KEY" : keys.apiKey
+      }
+    })
+    .then(r => r.json())
+    .then(parsed => renderAllBills(parsed.results[0].bills))
+  }
+
+  function renderSingleBill(bill){
+    return `<div class = "bill">
+              <h3 class = "bill-num"><a href = "bill-link">${bill.number}</a></h3>
+              <h4 class = "short-title">${bill.short_title}</h4>
+              <p class= "latest-action">${bill.latest_major_action}</p>
+              <br>
+              <span class= "introduced">Introduced ${bill.introduced_date}</span><span class= "sponsor">Sponsored by ${bill.sponsor_title} ${bill.sponsor_name}, ${bill.sponsor_party}-${bill.sponsor_state}</span>
+              <span class="rep-votes"></span>
+            </div>`
+  }
+
+  function renderAllBills(bills){
+    fetchedBills = bills.map(function(bill){
+      return renderSingleBill(bill)
+    }).join("")
+    billContainer.innerHTML = fetchedBills
+  }
 
   function fetchAllReps(){
     fetch(url)
@@ -33,12 +80,25 @@ document.addEventListener("DOMContentLoaded", (e)=>{
   }
 
   function renderAllReps(reps){
-    centerContainer.innerHTML = reps.map(function(rep){
+    fetchedReps = reps.map(function(rep){
       return renderSingleRep(rep)
     }).join("")
+    repContainer.innerHTML = fetchedReps
   }
 
-   fetchAllReps()
+  fetchAllReps()
+  fetchBills()
+  swapToBills()
 
 
-})
+   function swapToBills(){
+     repContainer.style.display = "none"
+     billContainer.style.display = "grid"
+   }
+
+   function swapToReps(){
+     repContainer.style.display = "grid"
+     billContainer.style.display = "none"
+   }
+
+}) //end of DOMContentLoaded
